7dd50764f6d23f77dd33beae969ae03d88c98155
Isha <xishaapatel@gmail.com>
2021-09-02 12:18:20 +0530

    feature: click

---
+++ app.js
@@ -0,0 +1,20 @@
+//input
+
+
+// var isha = prompt("Hey isha? Is it working");
+
+// var infotext = isha + " Congratulations on your first js";
+
+// //output
+// alert(infotext);
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click",function clickEventHandler() {
+    console.log("clicked");
+})
+
+
+var clickMe = document.querySelector("#click");
+clickMe.addEventListener("click" , function clickEventHandler() {
+    console.log("Bless you!");
+})
+
